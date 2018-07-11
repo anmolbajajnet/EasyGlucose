c964fdc0943b45b3889a129750e7544f8f605c72
anmolbajajnet <hello@anmolbajaj.net>
2018-07-11 16:08:01 -0700

    "Go back" button dismisses properly, linked the compose view controller to the file, added the log model

---
+++ EasyGlucose/Models/Log.swift
@@ -0,0 +1,9 @@
+//
+//  Log.swift
+//  EasyGlucose
+//
+//  Created by Anmol Bajaj on 2018-07-11.
+//  Copyright © 2018 Glucinators. All rights reserved.
+//
+
+import Foundation
---
+++ EasyGlucose/Views/ComposeViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ComposeViewController.swift
+//  EasyGlucose
+//
+//  Created by Anmol Bajaj on 2018-07-11.
+//  Copyright © 2018 Glucinators. All rights reserved.
+//
+
+import UIKit
+
+class ComposeViewController: UIViewController {
+
+    @IBAction func close() {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
