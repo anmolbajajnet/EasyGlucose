3a4462cc8920b208229362b8933bbe3885ab4b35
anmolbajajnet <hello@anmolbajaj.net>
2018-07-30 16:14:19 -0700

    Try Again

--- EasyGlucose/Views/EditProfileViewController.swift
+++ EasyGlucose/Views/EditProfileViewController.swift
@@ -6,9 +6,54 @@
 //
 
 import UIKit
+import RealmSwift
 
 class EditProfileViewController: UIViewController {
+    
+    @IBOutlet weak var userName: UITextField!
+    @IBOutlet weak var diabetesType: UISegmentedControl!
+    @IBOutlet weak var userEmail: UITextField!
+    @IBOutlet weak var practitionerEmail: UITextField!
+    
+    @IBAction func saveProfile(_ sender: Any) {
+        // delete previous profile
+        deletePreviousProfile()
+        // saving profile
+        let profileToBeSaved = Profile()
 
+        if diabetesType.selectedSegmentIndex == 0{
+            profileToBeSaved.diabetesType = "Type 1"
+        }else if diabetesType.selectedSegmentIndex == 1{
+            profileToBeSaved.diabetesType = "Type 2"
+        }else{
+            profileToBeSaved.diabetesType = "Gestational"
+        }
+        
+        if practitionerEmail.text != nil{
+            profileToBeSaved.practitionerEmail = practitionerEmail.text!
+        }
+        if userName.text != nil {
+            profileToBeSaved.userName = userName.text!
+        }
+        if userEmail.text != nil {
+            profileToBeSaved.userEmail = userEmail.text!
+        }
+        let realm = try! Realm()
+        try! realm.write {
+            print("Saving data")
+            realm.add(profileToBeSaved)
+        }
+    }
+    
+    
+    func deletePreviousProfile(){
+        let realm = try! Realm()
+        let testDataToDelete = realm.objects(Profile.self)
+        try! realm.write({
+            realm.delete(testDataToDelete)
+        })
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -19,16 +64,5 @@ class EditProfileViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
--- EasyGlucose/Views/ProfileViewController.swift
+++ EasyGlucose/Views/ProfileViewController.swift
@@ -13,30 +13,115 @@ class ProfileViewController: UIViewController {
 
 
     @IBOutlet weak var avgLabel: UILabel!
+    @IBOutlet weak var avgCarbs: UILabel!
+    @IBOutlet weak var avgBP: UILabel!
+    
+    
+    @IBOutlet weak var nameDisplay: UILabel!
+    @IBOutlet weak var emailDisplay: UILabel!
+    @IBOutlet weak var typeDisplay: UILabel!
+    @IBOutlet weak var healthEmailDisplay: UILabel!
+    @IBOutlet weak var nameTopDisplay: UILabel!
     
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        fillProfile()
         avgCalculation()
-        
-        // Do any additional setup after loading the view.
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        avgCalculation()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
-    func avgCalculation() {
+        
+    func fillProfile(){
         let realm = try! Realm()
-        let allTime: Int = realm.objects(Log.self).sum(ofProperty: "measurement")
-        let mean = allTime/mainInstance.logArrayCount
-        avgLabel.text = String(mean)
+        let profile = realm.objects(Profile.self)
+        let profileArr = profile.map({$0})
+        if(profileArr.count == 1){
+            nameDisplay.text = profileArr[0].userName
+            emailDisplay.text = profileArr[0].userEmail
+            typeDisplay.text = profileArr[0].diabetesType
+            healthEmailDisplay.text = profileArr[0].practitionerEmail
+            nameTopDisplay.text = profileArr[0].userName
+        }
     }
     
+    func avgCalculation() {
+        if mainInstance.logArrayCount == 0 {
+            let zero = 0
+            avgLabel.text = String(zero)
+            avgCarbs.text = String(zero)
+            avgBP.text = String(zero)
+            
+        }
+        else {
+            let realm = try! Realm()
+            let data = realm.objects(Log.self)
+            let dataArr = data.map({$0})
+            var validCarbCount = 0
+            var carbSum = 0
+            var validBloodPressureCount = 0
+            var bloodPressureSum = 0
+            var glucoseSum = 0
+            
+            
+            for logObj in dataArr {
+                
+                if Int(logObj.carbMeasurement) != nil{
+                    if Int(logObj.carbMeasurement)! != 0 {
+                        validCarbCount += 1
+                        carbSum += Int(logObj.carbMeasurement)!
+                    }
+                }
+                if Int (logObj.bloodPressureMeasurement) != nil{
+                    if Int(logObj.bloodPressureMeasurement) != 0{
+                        validBloodPressureCount += 1
+                        bloodPressureSum += Int(logObj.bloodPressureMeasurement)!
+                    }
+                }
+                glucoseSum += logObj.measurement
+            }
+            print(validCarbCount)
+            print(validBloodPressureCount)
+            
+            avgLabel.text = String(glucoseSum / dataArr.count)
+            if validCarbCount != 0 {
+                avgCarbs.text = String(carbSum / validCarbCount)
+            }else {
+                avgCarbs.text = String(0)
+            }
+            if validBloodPressureCount != 0 {
+                avgBP.text = String(bloodPressureSum/validBloodPressureCount)
+            }else{
+                avgBP.text = String(0)
+            }
+            
+            
+            
+            
+            
+            
+//            let realm = try! Realm()
+//            let allTime: Int = realm.objects(Log.self).sum(ofProperty: "measurement")
+//            let mean = allTime/mainInstance.logArrayCount
+//            avgLabel.text = String(mean)
+            
 
+        }
 
-
+    }
 }
+
+    
+
+
+
+
